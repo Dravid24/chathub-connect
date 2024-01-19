a6d086e37a55e8a7ea96aa70a070abc79c0912ec
Dravid24 <dravidmani1998@gmail.com>
2024-01-19 23:30:34 +0530

    update vite config for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["dependency-to-exclude"],
+  },
   server: {
     port: 3000,
     proxy: {
