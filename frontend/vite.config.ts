7f046936dda951f4a22cdd4fceeffcdd3d30677a
Dravid24 <dravidmani1998@gmail.com>
2024-01-19 23:34:37 +0530

    excluse react-dom client in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ["dependency-to-exclude"],
+    exclude: ["react-dom/client"],
   },
   server: {
     port: 3000,
