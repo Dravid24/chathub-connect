59857ba5523621fdb5974b971339260adfed99eb
Dravid24 <dravidmani1998@gmail.com>
2024-04-09 13:39:59 +0530

    Implement real time chating using socket IO

--- frontend/src/component/MessageDetails.tsx
+++ frontend/src/component/MessageDetails.tsx
@@ -7,23 +7,33 @@ import {
   useToast,
   Avatar,
   Tooltip,
+  Image,
 } from "@chakra-ui/react";
 import { IoMdSend } from "react-icons/io";
 import { ChatState } from "../Context/ChatProvider";
 import axios from "axios";
 import ScrollableFeed from "react-scrollable-feed";
-import { m } from "framer-motion";
 import {
   isSameSender,
   isLastMessage,
   isSameSenderMargin,
   isSameUser,
 } from "../common/util";
+import io from "socket.io-client";
+import typingIcon from "../assets/typing.gif";
+
+// const ENDPOINT = "http://localhost:5000"; // development
+const ENDPOINT = "https://chathub-connect.onrender.com"; // production
+let socket, selectedChatCompare;
 
 const MessageDetails = ({ user }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [currentMsg, setCurrentMsg] = useState("");
   const [allMessages, setAllMessages] = useState([]);
+  const [socketConnection, setSocketConnection] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+
   const { selectedChat } = ChatState();
 
   const toast = useToast();
@@ -35,8 +45,30 @@ const MessageDetails = ({ user }) => {
   };
 
   useEffect(() => {
+    socket = io(ENDPOINT);
+    socket.emit("setup", user);
+    socket.on("connected", () => setSocketConnection(true));
+    socket.on("typing", () => setIsTyping(true));
+    socket.on("stop typing", () => setIsTyping(false));
+  }, []);
+
+  useEffect(() => {
     getAllMessage();
+    selectedChatCompare = selectedChat;
   }, [selectedChat]);
+
+  useEffect(() => {
+    socket.on("message recieved", (msgRecieved) => {
+      if (
+        !selectedChatCompare ||
+        selectedChatCompare._id != msgRecieved.chat._id
+      ) {
+        // notification
+      } else {
+        setAllMessages([...allMessages, msgRecieved]);
+      }
+    });
+  });
 
   const getAllMessage = async () => {
     if (!selectedChat) return;
@@ -46,6 +78,8 @@ const MessageDetails = ({ user }) => {
       .then((res) => {
         setIsLoading(false);
         setAllMessages(res.data);
+
+        socket.emit("join chat", selectedChat._id);
       })
       .catch(() => {
         toast({
@@ -61,6 +95,7 @@ const MessageDetails = ({ user }) => {
   const handleSendMessage = async (e) => {
     if ((e.key === "Enter" || e.key === undefined) && currentMsg) {
       setCurrentMsg("");
+      socket.emit("stop typing", selectedChat._id);
       axios
         .post(
           "/api/message",
@@ -73,6 +108,7 @@ const MessageDetails = ({ user }) => {
         )
         .then((res) => {
           const data = res.data;
+          socket.emit("send message", data);
           setAllMessages([...allMessages, data]);
         })
         .catch(() => {
@@ -89,6 +125,24 @@ const MessageDetails = ({ user }) => {
   };
   const handleMessageChange = (e) => {
     setCurrentMsg(e.target.value);
+
+    if (!socketConnection) return;
+
+    if (!typing) {
+      setTyping(true);
+      socket.emit("typing", selectedChat._id);
+    }
+    const lastTypingTime = new Date().getTime();
+    const timer = 3000;
+    setTimeout(() => {
+      const timeNow = new Date().getTime();
+      const timeDiff = timeNow - lastTypingTime;
+
+      if (timeDiff >= timer && typing) {
+        socket.emit("stop typing", selectedChat._id);
+        setTyping(false);
+      }
+    }, timer);
   };
 
   return (
@@ -152,6 +206,9 @@ const MessageDetails = ({ user }) => {
             ))}
           </ScrollableFeed>
         </div>
+      )}
+      {isTyping && (
+        <Image src={typingIcon} alt="Typing..." w={16} ml="25px" mb="-10px" />
       )}
       <FormControl
         onKeyDown={handleSendMessage}
